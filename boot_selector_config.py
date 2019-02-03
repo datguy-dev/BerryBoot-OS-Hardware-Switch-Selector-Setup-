a759dd4a66ca1fd5bfb54444ba1f5f243e4e2adc
datguy-dev <dev.deque@gmail.com>
2019-02-02 19:35:18 -0500

    Add files via upload

---
+++ boot_selector_config.py
@@ -0,0 +1,13 @@
+gpio_image = {
+	#gpio / image name
+	14 : 'Raspbian.Placeholder1',
+	15 : 'Raspbian.Placeholder2'
+}
+
+#ftp settings
+ftp_user = 'pi'
+ftp_pass = 'raspberry'
+ftp_port = 21
+
+#if successful, this is the value for a countdown timer
+countdown = 20
