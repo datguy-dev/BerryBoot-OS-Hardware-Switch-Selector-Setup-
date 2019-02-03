8b953324aad0be3d7981203f014cbe3453fe6f48
datguy-dev <dev.deque@gmail.com>
2019-02-02 19:16:23 -0500

    Add files via upload

---
+++ boot_selector_config.py
@@ -0,0 +1,13 @@
+gpio_image = {
+	#gpio / image name
+	14 : 'Raspbian.Jesse.Base.img256',
+	15 : 'retropie-4.4-rpi2_rpi3-berryboot.img256'
+}
+
+#ftp settings
+ftp_user = 'pi'
+ftp_pass = 'raspberry'
+ftp_port = 21
+
+#if successful, this is the value for a countdown timer
+countdown = 20
